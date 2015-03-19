804a3ab91e2554099ab2cbb1e5ce7b517ed47c87
sarahdurant <sarah.ash.durant@gmail.com>
2015-03-18 22:25:50 -0600

    Create form implemented

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,59 @@
+@extends('app')
+
+@section('content')
+
+<h1> Create</h1>
+
+{!! Form::open(['url' => 'events']) !!}
+    <div class="form-group">
+        {!! Form::label('title', 'Title:') !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('description', 'Description:') !!}
+        {!! Form::text('description', null, ['class' => 'form-control']) !!}
+    </div>
+    {{-- see http://eonasdan.github.io/bootstrap-datetimepicker/ --}}
+    <div class="form-group">
+        {!! Form::label('startDate', 'Start Date:') !!}
+        <div class='input-group date' id='startDateTimePicker'>
+            <input type='text' class="form-control" />
+            <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+            </span>
+        </div>
+    </div>
+    <div class="form-group">
+        {!! Form::label('endDate', 'End Date:') !!}
+        <div class='input-group date' id='endDateTimePicker'>
+            <input type='text' class="form-control" />
+                <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+        </div>
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Create', ['class' => 'btn btn-primary form-control']) !!}
+    </div>
+{!! Form::close() !!}
+
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+<script>
+    $(function () {
+        $('#startDateTimePicker').datetimepicker();
+        $('#endDateTimePicker').datetimepicker();
+        $("#startDateTimePicker").on("dp.change",function (e) {
+            $('#endDateTimePicker').data("DateTimePicker").minDate(e.date);
+        });
+        $("#endDateTimePicker").on("dp.change",function (e) {
+            $('#startDateTimePicker').data("DateTimePicker").maxDate(e.date);
+        });
+    });</script>
+@stop

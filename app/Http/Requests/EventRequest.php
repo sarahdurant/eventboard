e5103769d499a19a8e0bcf2255eaab515c56f3f9
Sarah Durant <sarah.ash.durant@gmail.com>
2015-03-23 00:36:46 +0000

    Generalized request handler for events

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -2,7 +2,7 @@
 
 use App\Http\Requests\Request;
 
-class CreateEventRequest extends Request {
+class EventRequest extends Request {
 
 	/**
 	 * Determine if the user is authorized to make this request.

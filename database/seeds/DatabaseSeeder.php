e5c0f7b419ffed8f7fd8f96c6990d3d090b60e47
sarahdurant <sarah.ash.durant@gmail.com>
2015-03-24 21:50:53 -0600

    -Added seeding for the Users and Events tables with faked data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Event;
+
 
 class DatabaseSeeder extends Seeder {
 
@@ -14,7 +17,77 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+        $user = User::create(array(
+            'name' => 'sarahdurant',
+            'email' => 'sarah@example.com',
+            'isOrganizer' => true,
+            'password' => 'qwerty'
+        ));
+        $this->call('UsersTableSeeder');
+        $this->call('EventsTableSeeder');
+        $this->call('TagsTableSeeder');
+        $this->call('EventsTagsTableSeeder');
+        $this->call('SubscriptionsTableSeeder');
 	}
 
+}
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        for ($i = 0; $i < 100; $i++)
+        {
+            $user = User::create(array(
+                'name' => $faker->userName,
+                'email' => $faker->email,
+                'isOrganizer' => $faker->boolean($chanceOfGettingTrue = 50),
+                'password' => $faker->word
+            ));
+        }
+    }
+}
+
+class EventsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        for ($i = 0; $i < 100; $i++)
+        {
+            $start = $faker->dateTimeBetween($startDate = 'now', $endDate = '+12 weeks');
+            $end = $start->add(new DateInterval('PT' . rand(1,8) . 'H'));
+
+            $event = Event::create(array(
+                'title' => $faker->text($maxNbChars = 40),
+                'description' => $faker->text($maxNbChars = 512),
+                'startDate' => $start,
+                'endDate' => $end,
+                'organizerID' => $faker->numberBetween($min = 1, $max = 100)
+            ));
+        }
+    }
+}
+
+class TagsTableSeeder extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
+class EventsTagsTableSeeder extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
+class SubscriptionsTableSeeder extends Seeder
+{
+    public function run()
+    {
+
+    }
 }

fb6a29f318294fd78fd4aca1362f909b299b8dbb
Sarah Durant <sarah.ash.durant@gmail.com>
2015-03-29 03:29:11 +0000

    Fixed Store method so it actually uses the logged in user's ID as organizerID

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -1,100 +1,101 @@
 <?php namespace App\Http\Controllers;
 
+use Auth;
 use App\Event;
 use App\Http\Requests;
 use App\Http\Requests\EventRequest;
 use App\Http\Controllers\Controller;
 
 
-class EventsController extends Controller {
+class EventsController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
 
         $events = Event::all();
 
         return view('events.index', compact('events'));
-	}
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('events.create');
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view('events.create');
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store(EventRequest $request)
-	{
-		 $event = new Event($request->all());
-		 Auth::user()->ownedEvents()->save($event);
-		 Event::create($request->all());
-	
-     return redirect('events');
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store(EventRequest $request)
+    {
+        $event = new Event($request->all());
+        $event->organizerID = Auth::id();
+        $event->save();
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$event = Event::findOrFail($id);
+        return redirect('events');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
 
         return view('events.show', compact('event'));
-	}
+    }
 
 
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$event = Event::findOrFail($id);
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $event = Event::findOrFail($id);
 
         return view('events.edit', compact('event'));
-	}
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id, EventRequest $request)
-	{
-		$event = Event::findOrFail($id);
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id, EventRequest $request)
+    {
+        $event = Event::findOrFail($id);
 
-		$event->update($request->all());
-	}
+        $event->update($request->all());
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
